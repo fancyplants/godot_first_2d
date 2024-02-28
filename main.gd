c1bafbf86a4aea838b4f1050a4da8bf87d4ed135
Joshua Flancer <jtflance@protonmail.com>
2024-02-27 23:52:14 -0800

    Add HUD with title screen, score, and game over

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +16,15 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout():
@@ -50,6 +54,7 @@ func _on_mob_timer_timeout():
 # increment the player's score by 1 per second they live
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 # start the gaaaaaaame
 func _on_start_timer_timeout():
